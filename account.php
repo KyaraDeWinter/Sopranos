b422406bcd488fabd3c6ad40ec91cd6849c80051
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2021-02-26 10:28:01 +0100

    first commit

---
+++ account.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- connectie css -->
+<link rel="stylesheet" href="css/stylesheet.css">
+<!-- connectie google fonts: Lato en Rock Salt -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
+<!-- connectie font awesome: fav-icons -->
+<script src="https://kit.fontawesome.com/5b23f4b030.js" crossorigin="anonymous"></script>
+
+<!-- titel icon en titel -->
+<link rel="icon" href="images/logo.png" type="image">
+<title>Sopranos Pizza | Vers bereid uit eigen keuken | Bestel nu</title>
+</head>
+<body>
+
+<header>
+    <!-- landingspage: image en navigatiebar -->
+    <section class="landing">
+        <div class="landing-img">
+            <div class="titel">
+                <div class="titel-h1">
+                <h1>Sopranos Pizza</h1>
+                </div>
+                <div class="titel-p">
+                <p>De lekkerste pizza's warm thuisbezorgd</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- navigatiebar met hamburger menu -->
+        <div class="navigatie">
+                <nav>
+                    <div class="hamburger">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </div>
+                    <!-- links naar id op webpagina (onepage) -->
+                    <ul class="nav-links">
+                        <li><a href="index.php"></a>Menukaart</li>
+                        <li><a href="index.php"></a>Over Ons</li>
+                        <!-- mijnaccount linkt naar aparte pagina -->
+                        <li><a href="#"></a>Mijn Account</li>
+                    </ul>
+                </nav>
+            </div>
+    </section> 
+</header>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,171 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- connectie css -->
+<link rel="stylesheet" href="css/stylesheet.css">
+<!-- connectie google fonts: Lato en Rock Salt -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
+<!-- connectie font awesome: fav-icons -->
+<script src="https://kit.fontawesome.com/5b23f4b030.js" crossorigin="anonymous"></script>
+
+<!-- titel icon en titel -->
+<link rel="icon" href="images/logo.png" type="image">
+<title>Sopranos Pizza | Vers bereid uit eigen keuken | Bestel nu</title>
+</head>
+<body>
+
+<header>
+    <!-- landingspage: image en navigatiebar -->
+    <section class="landing">
+        <div class="landing-img">
+            <div class="titel">
+                <div class="titel-h1">
+                <h1>Sopranos Pizza</h1>
+                </div>
+                <div class="titel-p">
+                <p>De lekkerste pizza's warm thuisbezorgd</p>
+                </div>
+            </div>
+        </div>
+    </section> 
+</header>
+
+<!-- welkom tekst -->
+<section class="welkom">
+    <div class="tekst-welkom">
+        <h1>Soprano's beste recepten elke dag vers bereid</h1>
+        <p>Door bij Sopranos een vers bereide pizza te bestellen maakt u een reis door Italie.</p>
+        <p>Buon Appetito!</p>
+    </div>
+
+    <div class="check-postcode">
+        <h3>Sopranos Pizza bezorgd in een aantal steden en omgeving van Rotterdam, Amsterdam en Utrecht. Check uw postcode en bestel!</h3>
+        <form action="index.php" method="get">
+            <!-- input van postcode, max 6 -->
+            <input type="text" name="postcode" maxlength="6" placeholder="bijv. 1234AB">
+            <input type="submit">
+        </form>
+    </div>
+</section>
+
+<!-- menukaart interactief -->
+<section id="menukaart" class="menukaart">
+<div class="menukaart-tekst">
+        <h4>Menu</h4>
+        <p>Bestel nu onze overheerlijke pizza's!</p>
+    </div>
+    <div class="menukaart-tabel">
+        <table>
+            <?php
+            $conn = mysqli_connect("localhost", "root", "", "sopranos");
+            $sql = "SELECT * FROM menu";
+            $result = $conn-> query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result-> fetch_assoc()) {
+                    echo "<tr><td>" . $row["product"] . "</td><td>" . $row["omschrijving"] . "</td></tr>";
+                }
+            } else {
+                echo "Geen Resultaat";
+            }
+            $conn->close();
+            ?>
+        </table>
+    </div>
+</section>
+
+<hr>
+
+<!-- over ons tekst -->
+<section id="overons" class="overons">
+    <div class="overons-tekst">
+        <h4>Over Ons</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo repellat eum ipsam quos itaque non tempora veritatis optio sequi velit hic dolorem quasi doloremque molestiae harum eaque nam saepe doloribus sapiente, enim voluptatum facilis minus blanditiis! Eaque eius ipsam doloribus. Iure error pariatur labore quisquam blanditiis modi est facere recusandae eveniet ullam possimus consequuntur minima, fugit fugiat beatae vel minus quibusdam eos corporis doloremque expedita!<br><br>Illum omnis libero mollitia temporibus, nobis, voluptate minima odio eum id ducimus ipsum, aliquid repudiandae sint amet. Accusantium fugiat deserunt dolore libero dolor aliquid, ab, labore rerum aspernatur minus veniam obcaecati eligendi porro mollitia?</p>
+    </div>
+</section>
+
+<hr>
+
+<!-- openingstijden tabel -->
+<section id="openingstijden" class="openingstijden">
+    <div class="openingstijden-tekst">
+        <h4>Openingstijden</h4>
+        <p>U kunt nog een half uur voor sluitingstijd bij ons een maaltijd bestellen, telefonisch of online.</p>
+    </div>
+    <div class="openingstijden-tabel"  style="text-align:center;">
+    <table>
+        <tbody>
+            <tr>
+                <th>Maandag</th>
+                <td>
+                    <div class="tijd">Gesloten</div>
+                </td>
+            </tr>
+            <tr>
+                <th>Dinsdag</th>
+                <td>
+                    <div class="tijd">Gesloten</div>
+                </td>
+            </tr>
+            <tr>
+                <th>Woensdag</th>
+                <td>
+                    <div class="tijd">Gesloten</div>
+                </td>
+            </tr>
+            <tr>
+                <th>Donderdag</th>
+                <td>
+                    <div class="tijd">17:00 - 20:45</div>
+                </td>
+            </tr>
+            <tr>
+                <th>Vrijdag</th>
+                <td>
+                    <div class="tijd">17:00 - 23:00</div>
+                </td>
+            </tr>
+            <tr>
+                <th>Zaterdag</th>
+                <td>
+                    <div class="tijd">17:00 - 23:00</div>
+                </td>
+            </tr>
+            <tr>
+                <th>Zondag</th>
+                <td>
+                    <div class="tijd">17:00 - 22:00</div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+</section>
+
+<section id="betalingsopties" class="betalingsopties">
+    <div class="betalingsopties-tekst">
+        <h4>Beschikbare betalingsopties</h4>
+        <p>Wanneer u bij ons een maaltijd bestelt kunt u uit verschillende betalingsopties kiezen.</p>
+    </div>
+    <div class="betalingsopties-icons">
+        <ul>
+            <li>
+                <i class="fas fa-credit-card"></i>
+                <span>Contactloos betalen</span>
+            </li>
+            <li>
+                <i class="fas fa-coins"></i>
+                <span>Contant betalen</span>
+            </li>
+            <li>
+                <i class="fab fa-ideal"></i>
+                <span>Betalen via IDeal</span>
+            </li>
+        </ul>
+    </div>
+</section>
+
+</body>
+</html>
